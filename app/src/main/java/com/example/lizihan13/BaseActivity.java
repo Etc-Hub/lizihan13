a46da7227eb421f400343df6d16dc16233404b01
Your Name <you@example.com>
2020-06-29 15:37:47 +0800

    Initial commit

---
+++ app/src/main/java/com/example/lizihan13/BaseActivity.java
@@ -0,0 +1,11 @@
+package com.example.lizihan13;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+    void initView(){};
+
+    void initData(){};
+
+}
